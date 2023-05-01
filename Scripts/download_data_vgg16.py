93830bfb0408d005a8b124805d22ab7d8b6ef3dc
TristanGollmart <tgollmart@gmail.com>
2023-05-01 12:40:32 +0200

    transfer learning

---
+++ Scripts/download_data_vgg16.py
@@ -0,0 +1,33 @@
+#!pip install tqdm
+
+import requests
+import math
+import os
+import zipfile
+from tqdm import tqdm_notebook as tqdm
+
+if not os.path.exists(os.path.join(r"..\\data\\", "PetImages")):
+    url = "https://downloads.codingcoursestv.eu/037%20-%20neuronale%20netze/PetImages.zip"
+    # Streaming, so we can iterate over the response.
+    r = requests.get(url, stream=True)
+
+    # Total size in bytes.
+    total_size = int(r.headers.get('content-length', 0));
+    block_size = 1024
+
+    print("Downloading...")
+    with open(os.path.join("data", "PetImages.zip"), 'wb') as f:
+        for data in tqdm(r.iter_content(block_size), total=math.ceil(total_size // block_size), unit='KB',
+                         unit_divisor=1024, unit_scale=True):
+            f.write(data)
+
+    print("Download completed")
+    print("Extracting...")
+
+    zip_ref = zipfile.ZipFile(os.path.join("data", "PetImages.zip"), 'r')
+    zip_ref.extractall(os.path.join("data"))
+    zip_ref.close()
+
+    print("Done!")
+else:
+    print("Datei existiert bereits")
--- Scripts/transfer_learning_vgg16.py
+++ Scripts/transfer_learning_vgg16.py
@@ -1,7 +1,20 @@
 # uses the google vgg16 net for image classification
 # on the imagenet challenge
 
-from keras.utils import load_img
+from keras.utils import load_img, img_to_array
+from keras.applications.vgg16 import preprocess_input, VGG16, decode_predictions
+from matplotlib import pyplot as plt
 
-img = load_img('..\data\dog.jpg')
+img = load_img('..\data\dog.jpg', target_size=(224, 224))
+plt.imshow(img)
+plt.show()
+
+# process image for vgg16 use
+img = img_to_array(img)
+img = img.reshape((1, 224, 224, 3))
+img = preprocess_input(img)
 print(img)
+
+model = VGG16()
+ypred = model.predict(img)
+ypred = decode_predictions(ypred)
