561df3e6a1d09e994d4c7891101c181275eec117
TristanGollmart <tgollmart@gmail.com>
2023-05-02 21:16:58 +0200

    transfer learning

--- Scripts/download_data_vgg16.py
+++ Scripts/download_data_vgg16.py
@@ -16,16 +16,16 @@ if not os.path.exists(os.path.join(r"..\\data\\", "PetImages")):
     block_size = 1024
 
     print("Downloading...")
-    with open(os.path.join("data", "PetImages.zip"), 'wb') as f:
-        for data in tqdm(r.iter_content(block_size), total=math.ceil(total_size // block_size), unit='KB',
-                         unit_divisor=1024, unit_scale=True):
+    with open(os.path.join(r"..\\data\\", "PetImages.zip"), 'wb') as f:
+        for data in r.iter_content(block_size):  #, total=math.ceil(total_size // block_size), unit='KB',
+                                                # unit_divisor=1024, unit_scale=True):
             f.write(data)
 
     print("Download completed")
     print("Extracting...")
 
-    zip_ref = zipfile.ZipFile(os.path.join("data", "PetImages.zip"), 'r')
-    zip_ref.extractall(os.path.join("data"))
+    zip_ref = zipfile.ZipFile(os.path.join(r"..\\data\\", "PetImages.zip"), 'r')
+    zip_ref.extractall(os.path.join(r"..\\data\\"))
     zip_ref.close()
 
     print("Done!")
--- Scripts/transfer_learning_vgg16.py
+++ Scripts/transfer_learning_vgg16.py
@@ -1,6 +1,7 @@
 # uses the google vgg16 net for image classification
 # on the imagenet challenge
 
+from keras.preprocessing.image import ImageDataGenerator
 from keras.utils import load_img, img_to_array
 from keras.applications.vgg16 import preprocess_input, VGG16, decode_predictions
 from matplotlib import pyplot as plt
@@ -15,6 +16,11 @@ img = img.reshape((1, 224, 224, 3))
 img = preprocess_input(img)
 print(img)
 
+# image augmentation
+gen = ImageDataGenerator()
+train_generator =
+
+# prediction
 model = VGG16()
 ypred = model.predict(img)
 ypred = decode_predictions(ypred)
