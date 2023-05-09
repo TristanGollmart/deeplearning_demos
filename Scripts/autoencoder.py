80f9ee2c956ffc0e950c97eec741d8846f04c258
TristanGollmart <tgollmart@gmail.com>
2023-05-09 20:37:03 +0200

    autoencoder

--- Scripts/autoencoder.py
+++ Scripts/autoencoder.py
@@ -3,9 +3,10 @@ import tensorflow as tf
 import gzip
 import numpy as np
 from keras.models import Model
-from keras.layers import Input, Flatten, Dropout, Dense, MaxPool2D, Conv2D
+from keras.layers import Input, Flatten, Dropout, Dense, MaxPool2D, Conv2D, UpSampling2D
 from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 # image transformations
 
@@ -26,14 +27,49 @@ def open_labels(filename):
 # get fashionMNIST data
 
 
-X_train = open_images("../data/fashion/train-images-idx3-ubyte.gz")
-y_train = open_labels("../data/fashion/train-labels-idx1-ubyte.gz")
+Y_train = open_images("../data/mnist/train-images-idx3-ubyte.gz")
+#X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+Y_test = open_images("../data/mnist/t10k-images-idx3-ubyte.gz")
 
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+# plt.imshow(X_train[0])
+# plt.show()
 
-X_test = open_images("../data/fashion/t10k-images-idx3-ubyte.gz")
-y_test = open_labels("../data/fashion/t10k-labels-idx1-ubyte.gz")
+# target: clean images, inputs: noisy images
+Y_train = Y_train.astype(np.float32) / 255.
+Y_test = Y_test.astype(np.float32) / 255.
+X_train = Y_train + np.random.normal(0, 0.2, size=Y_train.shape)
+X_test = Y_test + np.random.normal(0, 0.2, size=Y_test.shape)
 
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
-y_val = to_categorical(y_val)
+input = Input(shape=(28, 28, 1))
+encode = Conv2D(filters=1, kernel_size=(3, 3), padding='same', input_shape=(28, 28, 1), activation='relu')(input)
+encode = MaxPool2D(pool_size=(2, 2))(encode)
+
+decode = Conv2D(5, kernel_size=(3, 3), padding='same', activation='relu')(encode)
+decode = UpSampling2D(size=(2, 2))(decode)
+decode = Conv2D(1, kernel_size=(3, 3), padding='same', activation='sigmoid')(decode)
+
+model = Model(input, decode)
+
+
+model.compile(optimizer='adam', loss='mse')
+history = model.fit(X_train.reshape(-1, 28, 28, 1), Y_train.reshape(-1, 28, 28, 1), epochs=10, batch_size=32)
+model.save(r'..\models\mnist_autoencoder')
+
+model_encoder = Model(input, encode)
+
+# use auto encoder to remove noise from a test image
+
+image_test = X_train[0]
+image_restored = model.predict(image_test.reshape(1, 28, 28, 1))
+image_encoded = model_encoder.predict(image_test.reshape(1, 28, 28, 1))
+
+plt.imshow(image_test)
+plt.imshow(image_restored.reshape(28, 28))
+plt.imshow(image_encoded.reshape(14, 14))
+
+
+print(history.history)
+
+
+
+print("finished")
