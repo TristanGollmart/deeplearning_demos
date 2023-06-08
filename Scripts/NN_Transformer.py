61e390ce3a6f9c3924297a2f29bc4f296f58277f
got <tristan.gollmart@ompex.ch>
2023-06-08 17:45:37 +0200

    custom test models

--- Scripts/NN_Transformer.py
+++ Scripts/NN_Transformer.py
@@ -20,6 +20,9 @@ print(encode("hii there"))
 
 # encode the whole input text
 import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
 data = torch.tensor(encode(text), dtype=torch.long)
 print(data.shape)
 
@@ -27,3 +30,54 @@ print(data.shape)
 n = int(0.9*len(data))
 train_data = data[:n]
 val_data = data[n:]
+
+
+torch.manual_seed(42)
+
+# Example: split into chunks for statistical learning of different attention heads
+# on blocks of all different length up to block_size
+block_size = 8
+train_data[:block_size+1] # n+1 will lead to n chunks separate chunks of length 1,...,n: "h", "ha", "hal", ...
+def create_chunks(x ,y):
+    context = []
+    target = []
+    for t in range(block_size):
+        context.append(x[:t+1])
+        target.append(y[t])
+    return context, target
+
+context, target = create_chunks(train_data[:block_size], train_data[1:block_size+1])
+
+print(context)
+print(target)
+
+# split set into batches
+batch_size = 4
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    # random batch
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+# shapes of xb: [batch_size, block_size, ]
+xb, yb = get_batch('train')
+print(xb.shape, yb.shape)
+
+# BigramLanguageModel
+class BigramLanguageModel(nn.Module)
+    def __init__(self, vocab_size):
+        super().__init__()
+        # just get  token embedding from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets):
+        # embeds the (B,T)-Tensor into a (B,T,C) tensor, C is analog of channels in ConvNets
+        logits = self.token_embedding_table(idx)
+        return logits
+
+m = BigramLanguageModel(vocab_size)
+out = m(xb, yb)
+print(out.shape)
