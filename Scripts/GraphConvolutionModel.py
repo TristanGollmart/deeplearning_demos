69754359f7fee8de18fd559e6ced9b2088cffefd
got <tristan.gollmart@ompex.ch>
2023-06-30 13:49:40 +0200

    Graph Conv Net

--- Scripts/GraphConvolutionModel.py
+++ Scripts/GraphConvolutionModel.py
@@ -19,36 +19,52 @@ class FeedForward(nn.Module):
         super(FeedForward, self).__init__()
         self.ffd = nn.Sequential(
             nn.Linear(n_in, n_out),
-            nn.ReLU()
+            nn.ReLU(),
+            nn.Linear(n_out, n_out),
         )
 
     def forward(self, x):
         return self.ffd(x)
 
+class GraphConvLayer(nn.Module):
+    # implements a graph convolution layer
+    def __init__(self, adj, n_features, n_embd):
+        # Adjacency matrix A is expected to have self-connection
+        super(GraphConvLayer, self).__init__()
+        self.A = adj
+        self.D_sqr_inv = torch.diag(1/torch.sqrt(torch.sum(self.A, dim=1)))
+        self.A_hat = self.D_sqr_inv @ self.A @ self.D_sqr_inv
+        self.n_features = n_features
+        self.ffd1 = FeedForward(n_features, n_embd)
+        self.ffd2 = FeedForward(n_embd, n_features)
+        self.ln1 = nn.LayerNorm(n_features)
+        self.ln2 = nn.LayerNorm(n_embd)
+    def forward(self, x):
+        x = self.ffd1(self.ln(x))
+        x = self.A_hat @ x # message passing
+        x = self.ffd2(self.ln2(x))
+        return x
 
 class GraphConvolutionModel(nn.Module):
-    def __init__(self, A, n_features, n_embd=10):
+    # implements a graph convolutional neural network
+    def __init__(self, adj, n_features, n_embd=10):
         super(GraphConvolutionModel, self).__init__()
         self.n_data = np.shape(A)[0]
-        self.A = torch.tensor(A) + torch.diag(torch.ones(self.n_data))
+        self.A = torch.tensor(adj) + torch.diag(torch.ones(self.n_data))
         self.D_sqr_inv = torch.diag(1/torch.sqrt(torch.sum(self.A, dim=1)))
         self.A_hat = self.D_sqr_inv @ self.A @ self.D_sqr_inv
         self.n_features = n_features
         self.n_embd = n_embd
-        self.ln1 = nn.LayerNorm(n_features)
-        self.ln2 = nn.LayerNorm(n_embd)
-        self.ffd1 = FeedForward(n_features, n_embd)
-        self.ffd2 = FeedForward(n_embd, n_features)
+        self.gcn = GraphConvLayer(self.A_hat, self.n_features, self.n_embd)
+
 
     def forward(self, node_feats):
-        x = self.ffd(self.ln1(node_feats))
-        x = self.A_hat @ x  # message passing
+        x = self.gcn(x)
         # skip connection: x = x + node_feats
         return self.ffd2(self.ln2(x))
 
 graph = nx.karate_club_graph()
 labels = list(set([d['club'] for d in graph._node.values()]))
-
 labels_to_int = {label: i for i, label in enumerate(labels)}
 
 print(np.shape(graph.nodes))
