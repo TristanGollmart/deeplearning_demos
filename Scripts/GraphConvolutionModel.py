9c3167f1779728deca6105e45740a5753faa01d3
got <tristan.gollmart@ompex.ch>
2023-06-30 15:19:18 +0200

    Graph Conv Net

--- Scripts/GraphConvolutionModel.py
+++ Scripts/GraphConvolutionModel.py
@@ -2,8 +2,21 @@ import numpy as np
 import torch
 import torch.nn as nn
 import networkx as nx
-
+import torch.
 device = "cpu"
+
+
+def get_loss(y, ypred):
+    '''
+    :param y:
+    :param ypred:
+    :return:
+    '''
+    if len(np.unique(y)) == 2:
+        loss = nn.BCELoss()
+    else:
+        loss = nn.CrossEntropyLoss()
+    return loss(ypred, y)
 
 def make_adjacency(graph):
     n_nodes = len(graph)
@@ -37,10 +50,14 @@ class GraphConvLayer(nn.Module):
         self.n_features = n_features
         self.ffd1 = FeedForward(n_features, n_embd)
         self.ffd2 = FeedForward(n_embd, n_features)
-        self.ln1 = nn.LayerNorm(n_features)
-        self.ln2 = nn.LayerNorm(n_embd)
+        if n_features > 1:
+            self.ln1 = nn.LayerNorm(n_features)
+            self.ln2 = nn.LayerNorm(n_embd)
+        else:
+            self.ln1 = nn.Identity()
+            self.ln2 = nn.Identity()
     def forward(self, x):
-        x = self.ffd1(self.ln(x))
+        x = self.ffd1(self.ln1(x))
         x = self.A_hat @ x # message passing
         x = self.ffd2(self.ln2(x))
         return x
@@ -49,7 +66,7 @@ class GraphConvolutionModel(nn.Module):
     # implements a graph convolutional neural network
     def __init__(self, adj, n_features, n_embd=10):
         super(GraphConvolutionModel, self).__init__()
-        self.n_data = np.shape(A)[0]
+        self.n_data = np.shape(adj)[0]
         self.A = torch.tensor(adj) + torch.diag(torch.ones(self.n_data))
         self.D_sqr_inv = torch.diag(1/torch.sqrt(torch.sum(self.A, dim=1)))
         self.A_hat = self.D_sqr_inv @ self.A @ self.D_sqr_inv
@@ -59,7 +76,7 @@ class GraphConvolutionModel(nn.Module):
 
 
     def forward(self, node_feats):
-        x = self.gcn(x)
+        x = self.gcn(node_feats)
         # skip connection: x = x + node_feats
         return self.ffd2(self.ln2(x))
 
@@ -70,10 +87,28 @@ labels_to_int = {label: i for i, label in enumerate(labels)}
 print(np.shape(graph.nodes))
 print(graph.nodes)
 
-graph_features = torch.tensor([labels_to_int[d['club']] for d in graph._node.values()]) # simply label of which group member belongs to
+graph_features = torch.tensor([labels_to_int[d['club']] for d in graph._node.values()], dtype=float) # simply label of which group member belongs to
 graph_features = torch.unsqueeze(graph_features, dim=1)
 adj = make_adjacency(graph)
+
 model = GraphConvolutionModel(adj, n_features=1, n_embd=3)
+model.to(device, dtype=float)
 
 x_update = model(graph_features)
 print(x_update)
+
+# ------------------- TRAINING --------------------------------
+max_iters = 500
+lr = 5e-3
+eval_interval = 100
+
+opt = torch.optim.AdamW(model.parameters(), lr=lr)
+for iter in range(max_iters):
+    logits = model(graph_features)
+    loss = get_loss(logits, graph_features)
+    if iter % eval_interval == 0 or iter == max_iters-1:
+        print(f"step {iter}: train loss {loss:.4f}")
+
+    opt.zero_grad(set_to_none=True)
+    loss.backward()
+    opt.step()
