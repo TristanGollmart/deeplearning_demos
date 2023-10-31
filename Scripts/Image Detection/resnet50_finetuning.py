85aa5392779bc70f71646daf24f007c4d389503e
TristanGollmart <tgollmart@gmail.com>
2023-10-31 14:12:11 +0100

    resnet50 finetuning model with calssifier head to mnist data,

--- Scripts/Image Detection/resnet50_finetuning.py
+++ Scripts/Image Detection/resnet50_finetuning.py
@@ -13,8 +13,8 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-batch_size = 4
-
+batch_size = 100
+train_model = False
 
 def augment_image(img):
     # induce small shifts and rotations to mimic brain behaviour
@@ -135,12 +135,7 @@ resnet = models.resnet50(weights=resnet_weights).to(device)
 for parameter in resnet.parameters():
     parameter.requires_grad = False
 
-classifier = nn.Sequential(nn.Flatten(1),
-                           nn.Dropout(),
-                           nn.Linear(2048, 512),
-                           nn.ReLU(inplace=True),
-                           nn.Dropout(),
-                           nn.Linear(512, 512),
+classifier = nn.Sequential(nn.Linear(1000, 512),
                            nn.ReLU(inplace=True),
                            nn.Linear(512, 10)
                            )
@@ -148,7 +143,9 @@ classifier = nn.Sequential(nn.Flatten(1),
 print(list(resnet.modules()))
 print(classifier)
 
-feature_model = nn.Sequential(*list(resnet.children())[:-1])
+feature_model = resnet
+model = nn.Sequential(feature_model, classifier)
+model.to(device)
 
 # Get data
 transform_train = transforms.Compose([
@@ -157,7 +154,7 @@ transform_train = transforms.Compose([
         transforms.Lambda(lambda x: x if torch.is_tensor(x) else transforms.ToTensor()(x)),
         #transforms.Lambda(lambda x: x.unsqueeze_(1).repeat(1, 3, 1, 1)),
         #transforms.Lambda(lambda x: x.repeat(3, 1, 1) if len(x.shape) <= 2 else x.unsqueeze(1).repeat(1, 3, 1, 1)),
-        transforms.Lambda(lambda x: x.unsqueeze(0).expand(3, -1, -1) if len(x.shape) <= 2 else x.unsqueeze(1).expand(-1, 3, -1, -1)),
+        transforms.Lambda(lambda x: x.unsqueeze(0).expand(3, -1, -1) if len(x.shape) <= 2 else x.expand(3, -1, -1)), #batches are created automatically by dataloader: 3, N, N -> B, 3, N, N
         transforms.Resize((232, 232)),
         #transforms.RandomHorizontalFlip(),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
@@ -166,7 +163,7 @@ transform_train = transforms.Compose([
 transform_test = transforms.Compose([
         transforms.Lambda(lambda x: x if torch.is_tensor(x) else transforms.ToTensor()(x)),
         #transforms.Lambda(lambda x: x.unsqueeze_(1).repeat(1, 3, 1, 1)),
-        transforms.Lambda(lambda x: x.unsqueeze(0).expand(3, -1, -1) if len(x.shape) <= 2 else x.expand(3, -1, -1)),
+        transforms.Lambda(lambda x: x.unsqueeze(0).expand(3, -1, -1) if len(x.shape) <= 2 else (x.expand(3, -1, -1) if len(x.shape) == 3 else x.expand(-1, 3, -1, -1))),
         transforms.Resize((232, 232)),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
 ])
@@ -177,10 +174,10 @@ mnist = datasets.MNIST(root = "data",
                                 download=True)
 
 images = transform_train(mnist.data[:1].type(torch.float32))
-plt.imshow(images[0].permute(1, 2, 0))
+plt.imshow(images.permute(1, 2, 0))
 plt.imshow(mnist.data[0])
 
-def get_dataloaders(feature_model, transforms_train, transforms_test):
+def get_dataloaders(transforms_train, transforms_test):
     """
     returns dataloaders for train, validation and test data
     :param feature_model: pretrained model to create useful features/ embeddings
@@ -188,123 +185,81 @@ def get_dataloaders(feature_model, transforms_train, transforms_test):
     :param transforms_test:  torch transforms to apply to test data
     :return:
     """
-    feature_model.to(device)
+
     transform_PIL_to_NP = transforms.Lambda(lambda x: np.array(x))
-    train_data = datasets.MNIST(root = "data",
+    data = datasets.MNIST(root = "data",
                                 train = True,
-                                transform = transform_PIL_to_NP,
+                                transform = transforms.Compose([transform_PIL_to_NP, transform_train]),
                                 download=True)
 
-    train_data_subset = Subset(train_data, indices=range(400))
-
-    dataloader_temp = DataLoader(dataset=train_data_subset,
-                                batch_size=100,
-                                shuffle=True)
+    # train_dataloader = DataLoader(dataset=data,
+    #                             shuffle=True)
 
 
-    with torch.no_grad():
-        x = []
-        y = []
-        for i, data in enumerate(dataloader_temp, 0):
-            input, targets = data
-            targets = targets.to(device)
-            input = transforms_train(input.type(torch.float32).to(device))
-            y.append(torch.flatten(targets).tolist())
-            features = feature_model(input)
-            features = torch.flatten(features, 1).tolist()
-            x += features
-
-    y = np.reshape(y, -1)
-    y = torch.tensor(y).type(torch.long)
-    x = torch.tensor(x)
-    dataset_train = CustomDataset(x, y)
-
-    ndata = dataset_train.y.shape[0]
+    ndata = len(data) #train_dataloader.dataset.targets.size()[0]
     ntrain = int(round(0.9 * ndata, 0))
     nval = ndata - ntrain
-    train_subset, val_subset = torch.utils.data.random_split(dataset_train, [ntrain, nval])
+
+    train_subset, val_subset = torch.utils.data.random_split(data, [ntrain, nval])
 
     # Test Data
-
     test_data = datasets.MNIST(root="data",
                                train=False,
                                transform=transforms.Compose([transform_PIL_to_NP, transform_test]))
+    test_data_subset = Subset(test_data, indices=range(len(test_data)))
 
-    test_data_subset = Subset(test_data, indices=range(200))
-
-    # dataloader_temp = DataLoader(dataset=test_data_subset,
-    #                             batch_size=100,
-    #                             shuffle=True)
-    # with torch.no_grad():
-    #     x = []
-    #     y = []
-    #     for i, data in enumerate(dataloader_temp, 0):
-    #         input, targets = data
-    #         targets.to(device)
-    #         data = transforms_test(input.type(torch.float32).to(device))
-    #         y.append(torch.flatten(targets).tolist())
-    #         features = feature_model(data)
-    #         features = torch.flatten(features, 1).tolist()
-    #         x += features
-    #
-    # y = np.reshape(y, -1)
-    # y = torch.tensor(y).type(torch.long)
-    # x = torch.tensor(x)
-    # dataset_test = CustomDataset(x, y)
 
     dataloader_train = DataLoader(dataset= train_subset,
                                                    batch_size = batch_size,
                                                    shuffle = True)
-
     dataloader_val = DataLoader(dataset= val_subset,
                                                  batch_size=batch_size,
                                                  shuffle=False)
-
     dataloader_test = DataLoader(dataset= test_data_subset,
                                                    batch_size = batch_size,
                                                    shuffle = True)
 
     return dataloader_train, dataloader_val, dataloader_test
 
-dataloader_train, dataloader_val, dataloader_test = get_dataloaders(feature_model, transform_train, transform_test)
+dataloader_train, dataloader_val, dataloader_test = get_dataloaders(transform_train, transform_test)
 
 
 
 # training classifier head on extracted features
 criterion = nn.CrossEntropyLoss()
-opt = Adam(params=classifier.parameters(), lr=0.001)
 
-history = []
-for epoch in range(2):
-    running_loss = 0
-    for i, data in enumerate(dataloader_train, 0):
-        inputs, labels = data
+if train_model:
+    opt = Adam(params=classifier.parameters(), lr=0.0001)
+    history = []
+    for epoch in range(0):
+        running_loss = 0
+        for i, data in enumerate(dataloader_train, 0):
+            inputs, labels = data
 
-        opt.zero_grad()
-        probs = classifier(inputs)
-        loss = criterion(probs, labels)
-        loss.backward()
-        opt.step()
-        running_loss += loss.item()
-        history.append(loss.item())
-        if (i+1) % 1000 == 0:
-            print(f"epoch {epoch+1:d}, step {i+1:5d}, loss {running_loss/1000:.3f}")
-            running_loss = 0
+            opt.zero_grad()
+            probs = model(inputs)
+            loss = criterion(probs, labels)
+            loss.backward()
+            opt.step()
+            running_loss += loss.item()
+            history.append(loss.item())
+            if ((i+1) * batch_size) % 1000 == 0:
+                print(f"epoch {epoch+1:d}, step {i+1:5d}, loss {running_loss/ np.round(1000/batch_size):.3f}")
+                running_loss = 0
 
+    plt.plot(history)
+    plt.show()
+    torch.save(model, f"..\\..\\models\\resnet50_finetuned_Mnist.pth")
 
-
-plt.plot(history)
-plt.show()
-
-model = nn.Sequential(feature_model, classifier)
-torch.save(model, f"..\\..\\models\\resnet50_transfer_augmented_inference.pth")
+model = torch.load(f"..\\..\\models\\resnet50_finetuned_Mnist.pth")
 for i, data in enumerate(dataloader_test, 0):
     inputs, labels = data
     probs = model(inputs.type(torch.float32))
-    print(probs)
+    print(probs[0])
     break
 
 loss = evaluate_model(model, dataloader_test, criterion)
+loss_train = evaluate_model(model, dataloader_train, criterion)
 
 loss_augmented = evaluate_model(model, dataloader_test, criterion, True, aggregation_augmentations="avg")
 
